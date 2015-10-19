fbd8e8d04a95feb9802bfd9769d2980ee6fcf329
Paolo Payumo <paolo.payumo@yahoo.com>
2015-10-18 19:59:58 -0700

    Prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,14 +81,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'randomfantasy.herokuapp.com' }
   # Use smtp
   config.action_mailer.delivery_method = :smtp
-  #
+
   config.action_mailer.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    address: "smtp.sengrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 end
